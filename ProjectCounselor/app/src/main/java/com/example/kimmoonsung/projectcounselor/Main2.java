478577b5b619ca8d8a02d12e44ddd6ee556d7ed5
kimmoonsung <kimmoonsung@kimmoonsung-ui-MacBook-Pro.local>
2017-06-19 15:15:19 +0900

    UPDATE

--- ProjectCounselor/app/src/main/java/com/example/kimmoonsung/projectcounselor/Main2.java
+++ ProjectCounselor/app/src/main/java/com/example/kimmoonsung/projectcounselor/Main2.java
@@ -2,23 +2,64 @@ package com.example.kimmoonsung.projectcounselor;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector;
+
 
 /**
  * Created by kimmoonsung on 2017. 5. 28..
  */
 
-public class Main2 extends Activity{
+public class Main2 extends Activity implements OnGestureListener {
     ImageView Left,Right,Login_btn,Member_btn;
-    @Override
 
+    ////////////////////// swipe ////////////////////
+
+    private LinearLayout main;
+    private TextView viewA;
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    private GestureDetector gestureScanner;
+
+
+    //////////////////////      ////////////////////
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ////////////////////// swipe  ////////////////////
+
+        gestureScanner = new GestureDetector(this);
+        main = new LinearLayout(this);
+        main.setBackgroundColor(Color.GRAY);
+        main.setLayoutParams(new LinearLayout.LayoutParams(320, 480));
+
+        viewA = new TextView(this);
+        viewA.setBackgroundColor(Color.WHITE);
+        viewA.setTextColor(Color.BLACK);
+        viewA.setTextSize(30);
+        viewA.setGravity(Gravity.CENTER);
+        viewA.setLayoutParams(new LinearLayout.LayoutParams(320, 80));
+        main.addView(viewA);
+
+        ////////////////////////////////////////////////
+
+
         requestWindowFeature(Window.FEATURE_NO_TITLE); //타이틀바없애기
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, // 상태바없애기
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -73,5 +114,71 @@ public class Main2 extends Activity{
             }
         });
     }
+
+
+    ////////////////////// swipe  ////////////////////
+
+    @Override
+    public boolean onTouchEvent(MotionEvent me) {
+        return gestureScanner.onTouchEvent(me);
+    }
+
+    public boolean onDown(MotionEvent e) {
+        viewA.setText("-" + "DOWN" + "-");
+        return true;
+    }
+
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        try {
+            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                return false;
+
+
+// right to left swipe
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                Intent intent = new Intent(getApplicationContext(), Main3.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Left Swipe", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+// left to right swipe
+            else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                Intent intent = new Intent(getApplicationContext(), Main1.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Right Swipe", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        } catch (Exception e) {
+
+        }
+        return true;
+    }
+
+    public void onLongPress(MotionEvent e) {
+        Toast mToast = Toast.makeText(getApplicationContext(), "Long Press", Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        viewA.setText("-" + "SCROLL" + "-");
+        return true;
+    }
+
+    public void onShowPress(MotionEvent e) {
+        viewA.setText("-" + "SHOW PRESS" + "-");
+    }
+
+    public boolean onSingleTapUp(MotionEvent e) {
+        Toast mToast = Toast.makeText(getApplicationContext(), "Single Tap", Toast.LENGTH_SHORT);
+        mToast.show();
+        return true;
+    }
+
+
+
+    //////////////////////////////////////////
+
 
 }
